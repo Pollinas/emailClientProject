0793222f0921a946518f9f72f82477df1f8cb463
paulina szylin <paulina.szylin.programista@gmail.com>
2022-05-31 09:56:07 +0200

    Added options to delete messages and mark them as unread

--- src/com/barosanu/EmailManager.java
+++ src/com/barosanu/EmailManager.java
@@ -3,14 +3,19 @@ package com.barosanu;
 import com.barosanu.controller.services.FetchFoldersService;
 import com.barosanu.controller.services.FolderUpdaterService;
 import com.barosanu.model.EmailAccount;
+import com.barosanu.model.EmailMessage;
 import com.barosanu.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
 
     private FolderUpdaterService folderUpdaterService;
     //Folder handling:
@@ -31,10 +36,55 @@ public class EmailManager {
         folderUpdaterService.start();
     }
 
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
+
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
+    }
+
+    public void setRead() {
+        try{
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void setUnread() {
+        try{
+            selectedMessage.setRead(false);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, false);
+            selectedFolder.incrementMessagesCount();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteSelectedMessage() {
+        try{
+            selectedMessage.getMessage().setFlag(Flags.Flag.DELETED, true);
+            selectedFolder.getEmailMessages().remove(selectedMessage);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
